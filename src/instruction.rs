c6df2a2dfba35f9575214d5e2f1166d938e58b00
Ahmet Yıldırım <ahmet@spacerunners.com>
2021-12-14 21:19:20 +0300

    instruction.rs init

---
+++ src/instruction.rs
@@ -0,0 +1,10 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+
+use crate::state::*;
+
+// #[repr(C)]
+#[derive(Clone, Debug, BorshSerialize, BorshDeserialize, PartialEq)]
+pub enum AppInstruction {
+    /// Create a proposal
+    Create,
+}
